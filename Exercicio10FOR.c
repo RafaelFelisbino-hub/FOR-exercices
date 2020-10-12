6908d0e3a7b4010f326f232525f01a1745ac8759
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-12 17:21:16 -0300

    Update Exercicio10FOR.c

--- Exercicio10FOR.c
+++ Exercicio10FOR.c
@@ -1,3 +1,6 @@
+/*FaÃ§a um algoritmo para ler um valor X e calcular Y =
+X+2X+3X+4X+5X+...+20X.*/
+
 #include <stdio.h>
 #include <string.h>
 #include <conio.h>
@@ -7,26 +10,25 @@
 int main()
 {
 	float x = 0, y = 0;
-	int i,N;
+	int i = 0;
 
 	setlocale(LC_ALL, "Portuguese");
 	system("color 17");
 
-	printf("Digite o número de vezes para repetir a soma:  ");
-	scanf_s("%i", & N);
-
-	for (i = 0; i < N; i++)
+	printf("\n%d - Informe os valores de X, para nÃºmeros com casas decimais use a vÃ­rgula:  ", i);
+	scanf_s("%f", &x);
+	
+	for (i = 1; i <= 20; i++)
 	{
-		printf("\n%d - Informe os valores de X, para números com casas decimais use a vírgula:  ",i);
-		scanf_s("%f", &x);
-
-		if (x)
+		
+		if (i)
 		{
-			y += x;
+			y  = y + x * i;
 		}
+		
 	}
 
-	printf("\nO valor de Y é:  %.2f\n\n", y);
+	printf("\nO valor de Y Ã©:  %.2f\n\n", y);
 
 	system("pause");
 
